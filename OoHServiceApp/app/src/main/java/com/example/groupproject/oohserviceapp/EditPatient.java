127481ea37f87eed94345109468daeba90757145
LukeBev <cgb12174@uni.strath.ac.uk>
2017-11-14 15:37:41 +0000

    Adding Java classes for EditPatient and ViewPatient

---
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/EditPatient.java
@@ -0,0 +1,21 @@
+package com.example.groupproject.oohserviceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created on 14/11/2017.
+ */
+
+public class EditPatient extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editablepatient);
+    }
+}
