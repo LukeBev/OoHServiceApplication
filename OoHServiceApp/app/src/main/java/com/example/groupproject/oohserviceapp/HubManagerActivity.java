c4997e5a0e7c8c734320ba6e3696dc2da88d473f
LukeBev <cgb12174@uni.strath.ac.uk>
2017-11-14 13:05:30 +0000

    Tuesday Lab Changes

---
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/HubManagerActivity.java
@@ -0,0 +1,22 @@
+package com.example.groupproject.oohserviceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Luke on 14/11/2017.
+ */
+
+public class HubManagerActivity extends AppCompatActivity {
+
+    Button hubManBut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hubmanager);
+
+        hubManBut = (Button) findViewById(R.id.button);
+    }
+}
--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/MainActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.groupproject.oohserviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button hubManagerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hubManagerButton = (Button) findViewById(R.id.button2);
+
+        // Create click listener for trackSelectionButton
+        hubManagerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Track List Activity
+                Intent hubManScreen = new Intent(v.getContext(), HubManagerActivity.class);
+                startActivity(hubManScreen);
+            }
+        });
     }
 }
