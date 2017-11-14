1cc48b7f1de1b1534f46fb0adb0b96b866690245
LukeBev <cgb12174@uni.strath.ac.uk>
2017-11-14 13:39:37 +0000

    Adding the ClinicalTeam worker class and activity

---
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ClinicalTeam.java
@@ -0,0 +1,21 @@
+package com.example.groupproject.oohserviceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Luke on 14/11/2017.
+ */
+
+public class ClinicalTeam extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinicalteam);
+    }
+}
--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/MainActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button hubManagerButton;
+    Button hubManagerButton, clinicalTeamButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         hubManagerButton = (Button) findViewById(R.id.button2);
+        clinicalTeamButton = (Button) findViewById(R.id.button3);
 
-        // Create click listener for trackSelectionButton
+
+        // Create click listener for hub manager button
         hubManagerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start Track List Activity
                 Intent hubManScreen = new Intent(v.getContext(), HubManagerActivity.class);
                 startActivity(hubManScreen);
+            }
+        });
+
+        // Create click listener for clinical team button
+        clinicalTeamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Clinical team Activity
+                Intent clinicalTeamScreen = new Intent(v.getContext(), ClinicalTeam.class);
+                startActivity(clinicalTeamScreen);
             }
         });
     }
