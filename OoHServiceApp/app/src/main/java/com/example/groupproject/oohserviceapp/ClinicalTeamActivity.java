6d1f2430b72e009808890c57f6842bedf02a4f4b
B00012964 <33656677+B00012964@users.noreply.github.com>
2017-11-21 22:18:09 +0000

    Tally Ho

--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ClinicalTeamActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ClinicalTeamActivity.java
@@ -1,35 +1,52 @@
 package com.example.groupproject.oohserviceapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ResourceBundle;
 
 /**
  * Created on 14/11/2017.
+ * edited 21/11/2017.
  */
 
 public class ClinicalTeamActivity extends AppCompatActivity {
 
-    Button addNewPatientButton;
-
+    Button callHubManager;
+    ImageButton ctHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinicalteam);
 
-        addNewPatientButton = (Button) findViewById(R.id.button5);
-
+        callHubManager = (Button) findViewById(R.id.button5);
+        ctHomeButton = (ImageButton) findViewById(R.id.imageButtonct);
         // Create click listener for add new patient button
-        addNewPatientButton.setOnClickListener(new View.OnClickListener() {
+        callHubManager.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start Track List Activity
-                Intent EditPatient = new Intent(v.getContext(), EditPatientActivity.class);
-                startActivity(EditPatient);
+                // Start call
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:08000000000" ));
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
             }
         });
-    }
-}
+//Home button fixed
+                ctHomeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Go to Home Screen
+                        Intent HomeScreen = new Intent(v.getContext(), MainActivity.class);
+                        startActivity(HomeScreen);
+                    }
+                });
+            }
+        }
