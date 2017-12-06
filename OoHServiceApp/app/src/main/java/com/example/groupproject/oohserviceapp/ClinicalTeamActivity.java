2192a43b6e919198b31fd0ac14688b5f27de70a2
LukeBev <cgb12174@uni.strath.ac.uk>
2017-12-06 19:00:34 +0000

    Adding custom listView to clinical team class and activity

--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ClinicalTeamActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ClinicalTeamActivity.java
@@ -6,9 +6,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -18,8 +21,20 @@ import java.util.ResourceBundle;
 
 public class ClinicalTeamActivity extends AppCompatActivity {
 
+    private ArrayList<ArrayList<String>> myDetailLists = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<String>> customDetailLists = new ArrayList<ArrayList<String>>();
+    private ArrayList<String> tempfirstnameList = new ArrayList<String>();
+    private ArrayList<String> tempsurnameList = new ArrayList<String>();
+    private ArrayList<String> tempteamList = new ArrayList<String>();
+    private ArrayList<String> tempChiNumberList = new ArrayList<String>();
+    private ArrayList<String> tempAddressList = new ArrayList<String>();
+    private ArrayList<String> tempDobList = new ArrayList<String>();
+    private ArrayList<String> tempHeightList = new ArrayList<String>();
+    private ArrayList<String> tempWeightList = new ArrayList<String>();
+
     Button callHubManager;
     ImageButton ctHomeButton;
+    ListView patientListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +54,85 @@ public class ClinicalTeamActivity extends AppCompatActivity {
 
             }
         });
-                // Home button, linking to mainActivity
-                ctHomeButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Go to Home Screen
-                        Intent HomeScreen = new Intent(v.getContext(), MainActivity.class);
-                        startActivity(HomeScreen);
-                    }
-                });
+
+        // Home button, linking to mainActivity
+        ctHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to Home Screen
+                Intent HomeScreen = new Intent(v.getContext(), MainActivity.class);
+                startActivity(HomeScreen);
             }
+        });
+
+
+        getDatabase();
+    }
+
+    public void getDatabase() {
+
+        final DatabaseWorker dbwork = new DatabaseWorker(this);
+
+        // Fetching arrayLists from the Arraylist that was constructed with Database data
+        myDetailLists = dbwork.fetchDetails();
+
+        // Assigning each arrayList within the arrayList<arrayList<String>> to a specfic variable to manipulate
+        tempfirstnameList = myDetailLists.get(0);
+        tempsurnameList = myDetailLists.get(1);
+        tempteamList = myDetailLists.get(2);
+
+        // Creating a custom arrayList of arrayLists to pass to the custom created ListView adapter
+        for (int j = 0; j<tempfirstnameList.size(); j++) {
+
+            String f = tempfirstnameList.get(j);
+            String s = tempsurnameList.get(j);
+            String t = tempteamList.get(j);
+            ArrayList temp = new ArrayList<String>();
+            temp.add(f);
+            temp.add(s);
+            temp.add(t);
+
+            customDetailLists.add(temp);
+        }
+
+        // Continue assigning of arrayLists from arrayList<arrayList<String>>
+        tempChiNumberList = myDetailLists.get(3);
+        tempAddressList = myDetailLists.get(4);
+        tempDobList = myDetailLists.get(5);
+        tempHeightList = myDetailLists.get(6);
+        tempWeightList = myDetailLists.get(7);
+
+
+        // Create The Adapter with passing ArrayList<ArrayList<String>> as 3rd parameter
+        PatientListAdapter arrayAdapter =
+                new PatientListAdapter(this,R.layout.patient_list_items, customDetailLists);
+        // Set The Adapter
+        patientListView = (ListView) findViewById(R.id.clinical_names_list);
+        patientListView.setAdapter(arrayAdapter);
+
+        // Creating a listener for when an item from the generated list is selected
+        patientListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ClinicalTeamActivity.this, ViewPatientActivity.class);
+
+                // Storing details of the currently selected position in the list
+                intent.putExtra("firstName", tempfirstnameList.get(position));
+                intent.putExtra("surname", tempsurnameList.get(position));
+                intent.putExtra("team", tempteamList.get(position));
+                intent.putExtra("chiNumber", tempChiNumberList.get(position));
+                intent.putExtra("address", tempAddressList.get(position));
+                intent.putExtra("dob", tempDobList.get(position));
+                intent.putExtra("height" ,tempHeightList.get(position));
+                intent.putExtra("weight", tempHeightList.get(position));
+                intent.putExtra("position", position);
+
+                // Start ViewPatientActivity
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
         }
