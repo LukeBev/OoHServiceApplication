8abc710033d1c430a75b5eeaa581826ca6c2bdb6
LukeBev <cgb12174@uni.strath.ac.uk>
2017-12-06 16:56:17 +0000

    Updating patient activity class Updating patient activity class to retrieve and display data
    Updating patient activity class to retrieve and display data

--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ViewPatientActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/ViewPatientActivity.java
@@ -1,20 +1,74 @@
 package com.example.groupproject.oohserviceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
- * Created on 14/11/2017.
+ * Created on 01/12/2017.
  */
 
 public class ViewPatientActivity extends AppCompatActivity {
 
-
-
+    TextView firstName, surname, address, chiNumber, dob, height, weight, team;
+    ImageButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewablepatient);
+
+        // Getting the resource id for each textView
+        firstName = (TextView) findViewById(R.id.textView15);
+        surname = (TextView) findViewById(R.id.textView16);
+//        team = (TextView) findViewById(R.id.textAddress);
+        chiNumber = (TextView) findViewById(R.id.textView14);
+        address = (TextView) findViewById(R.id.textView17);
+        dob = (TextView) findViewById(R.id.textView18);
+        height = (TextView) findViewById(R.id.textView23);
+        weight = (TextView) findViewById(R.id.textView19);
+
+
+        // Unloading the bundle to get string values, to put data into appropriate textView
+        Bundle extras = getIntent().getExtras();
+
+        String firstnameString = extras.getString("firstName");
+        firstName.setText(firstnameString);
+
+        String surnameString = extras.getString("surname");
+        surname.setText(surnameString);
+
+//        String teamString = extras.getString("team");
+//        team.setText(teamString);
+
+        String chiNumberString = extras.getString("chiNumber");
+        chiNumber.setText(chiNumberString);
+
+        String addressString = extras.getString("address");
+        address.setText(addressString);
+
+        String dobString = extras.getString("dob");
+        dob.setText(dobString);
+
+        String heightString = extras.getString("height");
+        height.setText(heightString);
+
+        String weightString = extras.getString("weight");
+        weight.setText(weightString);
+
+
+        // Button to allow the user to navigate to the main activity of the application
+        homeButton = (ImageButton) findViewById(R.id.imageButtonpd1);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hi");
+                Intent HomeScreen = new Intent(v.getContext(), MainActivity.class);
+                startActivity(HomeScreen);
+            }
+        });
     }
 }
