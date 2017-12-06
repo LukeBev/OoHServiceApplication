c15810679a93194e715d5e85cf2d7d2c47d86940
LukeBev <cgb12174@uni.strath.ac.uk>
2017-12-06 18:06:46 +0000

    edit patient activity now is able to submit patient data
    Toast messages are also displayed dependant on input

--- OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/EditPatientActivity.java
+++ OoHServiceApp/app/src/main/java/com/example/groupproject/oohserviceapp/EditPatientActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 /**
@@ -16,6 +18,7 @@ public class EditPatientActivity extends AppCompatActivity {
 
     Button saveNewPatientButton;
     ImageButton epHomeButton;
+    EditText editTeamText, editCHIText, editFirstNameText, editSurnameText, editAddressText, editDobText, editHeightText, editWeightText;
 
 
     @Override
@@ -23,10 +26,60 @@ public class EditPatientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editablepatient);
 
+        editTeamText = (EditText) findViewById(R.id.editTextTeam);
+        editCHIText = (EditText) findViewById(R.id.editTextCHINumber);
+        editFirstNameText = (EditText) findViewById(R.id.editTextFName);
+        editSurnameText = (EditText) findViewById(R.id.editTextSurname);
+        editAddressText = (EditText) findViewById(R.id.editTextAddress);
+        editDobText = (EditText) findViewById(R.id.editTextDob);
+        editHeightText = (EditText) findViewById(R.id.editTextHeight);
+        editWeightText = (EditText) findViewById(R.id.editTextWeight);
+
+
+        final DatabaseWorker db = new DatabaseWorker(this);
+
+        String team, chi, firstName, surname, address, dob, height, weight;
+
         saveNewPatientButton = (Button) findViewById(R.id.buttonsavePatient);
-        epHomeButton = (ImageButton) findViewById(R.id.imageButtonep);
+
+        // Listener for when submit button clicked for inserting new patient to the database
+        saveNewPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String team = editTeamText.getText().toString();
+                String chi = editCHIText.getText().toString();
+                String firstName = editFirstNameText.getText().toString();
+                String surname = editSurnameText.getText().toString();
+                String address = editAddressText.getText().toString();
+                String dob = editDobText.getText().toString();
+                String height = editHeightText.getText().toString();
+                String weight = editWeightText.getText().toString();
+
+                // Checking for any empty fields before inserting to database
+                if (team.isEmpty() || chi.isEmpty() || firstName.isEmpty() || surname.isEmpty() || address.isEmpty() || dob.isEmpty() || height.isEmpty() || weight.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter all fields before adding patient!", Toast.LENGTH_LONG).show();
+                } else {
+                    // Method call to the database worker class, to allow for insertion to the database.
+                    db.insertDetails(team, chi, firstName, surname, address, dob, height, weight);
+                    Toast.makeText(getApplicationContext(), "Patient has been added to the system!", Toast.LENGTH_LONG).show();
+
+                    // Clear the editText fields
+                    editTeamText.setText("");
+                    editCHIText.setText("");
+                    editFirstNameText.setText("");
+                    editSurnameText.setText("");
+                    editAddressText.setText("");
+                    editDobText.setText("");
+                    editHeightText.setText("");
+                    editWeightText.setText("");
+                }
+            }
+        });
+
+
 
         // Create click listener for home button
+        epHomeButton = (ImageButton) findViewById(R.id.imageButtonep);
         epHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
